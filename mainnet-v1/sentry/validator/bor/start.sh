1afc2506169ace8d7583079d04f92ce7b9f5a89a
root <root@vmi939137.contaboserver.net>
2022-08-08 12:59:07 +0200

    init0

---
+++ mainnet-v1/sentry/validator/bor/start.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -x #echo on
+
+if [ -z "$1" ]
+  then
+    echo "Address is required as argument"
+  exit 1
+fi
+
+ADDRESS=$1
+BOR_DIR=${BOR_DIR:-~/.bor}
+DATA_DIR=$BOR_DIR/data
+
+bor --datadir $DATA_DIR \
+  --port 30303 \
+  --http --http.addr '127.0.0.1' \
+  --http.vhosts '*' \
+  --http.corsdomain '*' \
+  --http.port 8545 \
+  --ipcpath $DATA_DIR/bor.ipc \
+  --http.api 'eth,net,web3,txpool,bor' \
+  --networkid '1234' \
+  --syncmode 'full' \
+  --miner.gasprice '1000000000' \
+  --miner.gaslimit '20000000' \
+  --miner.gastarget '20000000' \
+  --txpool.nolocals \
+  --txpool.accountslots 16 \
+  --txpool.globalslots 32768 \
+  --txpool.accountqueue 16 \
+  --txpool.globalqueue 32768 \
+  --txpool.pricelimit '30000000000' \
+  --txpool.lifetime '1h30m0s' \
+  --keystore $BOR_DIR/keystore \
+  --unlock $ADDRESS \
+  --password $BOR_DIR/password.txt \
+  --allow-insecure-unlock \
+  --nodiscover --maxpeers 1 \
+  --metrics \
+  --pprof --pprof.port 7071 --pprof.addr '0.0.0.0' \
+  --mine
+  
